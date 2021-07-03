509a4e9b4284d74f13987c02fb8c34a67364d5b5
dlee-mitre <85962425+dlee-mitre@users.noreply.github.com>
2021-07-03 08:21:20 -0400

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,31 @@
+'''
+Write 6 print() statements to "draw" the following on the console
+
+ooo/\ooo
+oo/  \oo
+o/ '' \o
+/______\
+|| || ||
+xxxxxxxx
+
+(hint: use escape sequence)
+'''
+print()
+print()
+print()
+print()
+print()
+print()
+
+'''
+Can you do it with just one print statement?
+'''
+print()
+
+'''
+Choose three values for a, b, and c such that the final print() statement returns True
+'''
+a = 
+b = 
+c = 
+print(a**2 + b**2 == c**2)
